beb7264881fe3551ff25e13bfb9e0428242ae257
dennisphan <denis.phan262@gmail.com>
2021-08-04 07:06:28 -0600

    Create Python_practice-checkpoint.py

---
+++ Analysis projects/03 Election Analysis/.ipynb_checkpoints/Python_practice-checkpoint.py
@@ -0,0 +1,17 @@
+counties = ["Aparahoe", "Denver", "Jefferson"]
+
+print(counties)
+
+len(counties)
+
+print(counties[0:2])
+counties.append("El Paso")
+counties.insert(2, "El Paso")
+print(counties)
+
+my_tuple = tuple()
+my_tuple = ("first", 'second', 'third')
+
+len(my_tuple)
+type(my_tuple)
+print(my_tuple(1))
