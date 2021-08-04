e6383902e425a96b80f9fa63d697cf9c8afc5faf
dennisphan <denis.phan262@gmail.com>
2021-08-04 07:06:50 -0600

    Complete module 3.4

--- Analysis projects/03 Election Analysis/PyPoll.py
+++ Analysis projects/03 Election Analysis/PyPoll.py
@@ -1,15 +1,22 @@
+# Add our dependencies.
 import csv
-
-# Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
-election_data = open(file_to_load, 'r')
-
-# To do: perform analysis.
-print(election_data)
-
-# Close the file.
-election_data.close()
-
-pythonData
+with open(file_to_load) as election_data:
+    
+    # To do: read and analyze the data here.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
+    # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
