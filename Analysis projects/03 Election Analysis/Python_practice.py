b3c278ef294f46171cac3b507c2e73d305dba3fa
dennisphan <denis.phan262@gmail.com>
2021-08-04 07:06:57 -0600

    Update Python_practice.py

--- Analysis projects/03 Election Analysis/Python_practice.py
+++ Analysis projects/03 Election Analysis/Python_practice.py
@@ -1,7 +1,14 @@
-counties = ["Aparahoe", "Denver", "Jefferson"]
-print(counties)
-len(counties)
-print(counties[0:2])
-counties.append("El Paso")
-counties.insert(2, "El Paso")
-print(counties)
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
